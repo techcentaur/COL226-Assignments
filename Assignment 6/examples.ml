aa5100e97c49bf81edfdd34b7d899e9a374967e3
Ankit Solanki <ankit03june@gmail.com>
2018-04-22 22:47:09 +0530

    examples new: constructors changed

--- Assignment 6/examples.ml
+++ Assignment 6/examples.ml
@@ -1,68 +1,67 @@
 let program2 =
 [
     Fact(Atm("male", [N ("pandu", [])]));
-    Fact( N("male", [N ("arjun", [])]) );
-    Fact( N("male", [N ("nakul", [])]) );
-    Fact( N("female", [N ("kunti", [])]) );
-    Fact( N("female", [N ("madri", [])]) );
-    Fact( N("female", [N ("draupadi", [])]) );
+    Fact( Atm("male", [N ("arjun", [])]) );
+    Fact( Atm("male", [N ("nakul", [])]) );
+    Fact( Atm("female", [N ("kunti", [])]) );
+    Fact( Atm("female", [N ("madri", [])]) );
+    Fact( Atm("female", [N ("draupadi", [])]) );
 
-    Fact( N("married", [N ("pandu", []); N("kunti", [])] ));
-    Fact( N("married", [N ("pandu", []); N("madri", [])] ));
-    Fact( N("married", [N ("arjun", []); N("draupadi", [])] ));
-    Fact( N("married", [N ("nakul", []); N("draupadi", [])] ));
+    Fact( Atm("married", [N ("pandu", []); N("kunti", [])] ));
+    Fact( Atm("married", [N ("pandu", []); N("madri", [])] ));
+    Fact( Atm("married", [N ("arjun", []); N("draupadi", [])] ));
+    Fact( Atm("married", [N ("nakul", []); N("draupadi", [])] ));
 
     Rule(
-        N("married", [V "P"; V "Q"]),
+        Atm("married", [V "P"; V "Q"]),
         [
-            N("married", [V "Q"; V "P"])
+            Atm("married", [V "Q"; V "P"])
         ]
     );
 
     Rule(
-        N("wife", [V "A"; V "B"]),
+        Atm("wife", [V "A"; V "B"]),
         [
-            N("married", [V "A"; V "B"]);
-            N("male", [V "A"]);
-            N("female", [V "B"])
+            Atm("married", [V "A"; V "B"]);
+            Atm("male", [V "A"]);
+            Atm("female", [V "B"])
         ]
     );
 
     Rule(
-        N("cowife", [V "C"; V "D"]),
+        Atm("cowife", [V "C"; V "D"]),
         [
-            N("married", [V "E"; V "C"]);
-            N("married", [V "E"; V "D"]);
-            N("female", [V "C"]);
-            N("female", [V "D"]);
+            Atm("married", [V "E"; V "C"]);
+            Atm("married", [V "E"; V "D"]);
+            Atm("female", [V "C"]);
+            Atm("female", [V "D"]);
             (* X \= Y *)
         ]
     );
 
     Rule(
-        N("husband", [V "F"; V "G"]),
+        Atm("husband", [V "F"; V "G"]),
         [
-            N("married", [V "G"; V "F"]);
-            N("male", [V "G"]);
-            N("female", [V "F"])
+            Atm("married", [V "G"; V "F"]);
+            Atm("male", [V "G"]);
+            Atm("female", [V "F"])
         ]
     );
 
     Rule(
-        N("cohusband", [V "H"; V "I"]),
+        Atm("cohusband", [V "H"; V "I"]),
         [
-            N("married", [C "H"; C "J"]);
-            N("married", [C "I"; C "J"]);
-            N("male", [C "H"]);
-            N("male", [C "I"]);
-            (* X \= Y *)
+            Atm("married", [N("H",[]); N("J",[])]);
+            Atm("married", [N("I",[]); N("J",[])]);
+            Atm("male", [N("H",[])]);
+            Atm("male", [N("I",[])]);
         ]
     );
 ]
 
-let query2_1 = [N("male", [N("krishna",[])])];;
-let query2_2 = [N("married", [N("arjun",[]); V "X"])];;
-let query2_3 = [N("married", [V "X"; N("draupadi",[])])];;
-let query2_4 = [N("married", [V "X"; V "Y"])];;
-let query2_5 = [N("cohusband", [N("arjun",[]); N("nakul",[])]); N("cowife", [N("madri",[]); N("kunti",[])])];;
-let query2_6 = [N("cowife", [V "X"; N("kunti",[])])];;
+let query2_1 = [Atm("male", [N("krishna",[])])];;
+let query2_2 = [Atm("married", [N("arjun",[]); V "X"])];;
+let query2_3 = [Atm("married", [V "X"; N("draupadi",[])])];;
+let query2_4 = [Atm("married", [V "X"; V "Y"])];;
+let query2_5 = [Atm("cohusband", [N("arjun",[]); N("nakul",[])]); N("cowife", [N("madri",[]); N("kunti",[])])];;
+let query2_6 = [Atm("cowife", [V "X"; N("kunti",[])])];;
