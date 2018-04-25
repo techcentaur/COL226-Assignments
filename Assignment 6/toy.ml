beec5326bd9fc7e8b420a513384be037c888297d
Ankit Solanki <ankit03june@gmail.com>
2018-04-25 17:55:02 +0530

    DONE: Fuckin' done from toy interpreter

--- Assignment 6/toy.ml
+++ Assignment 6/toy.ml
@@ -1,137 +1,98 @@
-(* OCaml data type *)
-type symbol = string
-	and variable = string
-	and predicate = string
-		and program = clause list
-			and clause = Fact of head | Rule of rule
-				and head = Atm of atom
-					and rule = head*(head list)
-						and term = V of variable | N of symbol*(term list)
-							and atom = predicate*(term list)
-								and goal = head list;;
+(* Assignment-6: Toy interpreter language *)
 
+(* OCaml data types *)
+type constant = string;;
+type variable = string;;
+type predicate = string;;
+type term = C of constant | V of variable | A of predicate*(term list);;
+type atom = A of predicate*(term list);;
+type head = atom;;
+type body = atom list;;
+type clause = Fact of head | Rule of head*body;;
+type program = clause list;;
+type goal = atom list;;
 
-type substitution = (variable*term) list;;
+let solvedvar_pointer = ref [];;
 
-(*mergeing x in s*)
-(* val merge : 'a -> 'a list -> 'a list = <fun> *)
-let merge x s = if List.mem x s then s else s@[x];;
-
-
-(*unification*)
-(* val unification : 'a list -> 'a list -> 'a list = <fun> *)
 let rec unification s1 s2 = match s1 with
 | [] -> s2
-| x::x' -> merge x (unification x' s2);;
+| x::x' -> if (List.mem x s2) then unification x' s2 else (x::(unification x' s2));;
+
+let term2string term = match term with | V v -> v | C c -> c | _-> "";;
 
 
-(* subst *)
-(* val subst : term -> (variable * term) list -> term = <fun> *)
-exception TermNotFound;;
-let rec subst term s = match term with
-	V v -> (try List.assoc v s with | Not_found -> term)
-	| N (sym, []) -> N (sym, [])
-	| N(sym, termlist) -> if List.length termlist = 0 then term
-							  else N(sym, List.map ((fun s term -> subst term s) s) termlist);;
+let rec printsubs sub vars = match sub with
+    | [] -> ""
+    | (v, t) :: rest -> if List.exists (fun x -> x = v) vars then
+        (Printf.sprintf "\n%s = %s" v (term2string t)) ^ (printsubs rest vars)
+        else (printsubs rest vars) ;;
 
-(* val fst : 'a * 'b -> 'a  # Built-in function *)
-(* val compose : (variable * term) list -> substitution -> (variable * term) list = <fun> *)
-let rec compose s1 s2 = s2 @ (List.map ((fun s s' -> (fst s', subst (snd s') s)) s2) s1);;
+exception Not_found;;
 
+let rec occurs var term = match term with | V v -> v = var | C _ -> false | A (_, termlist) -> List.exists (occurs var) termlist ;;
+
+type substitution = variable*term;;
 exception Not_unifiable;;
 
-(* vars *)
-
-(* val fold_left : ('a -> 'b -> 'a) -> 'a -> 'b list -> 'a
-List.fold_left f a [b1; ...; bn] is f (... (f (f a b1) b2) ...) bn *)
-
-(* val fold_left2 : ('a -> 'b -> 'c -> 'a) -> 'a -> 'b list -> 'c list -> 'a *)
-(* List.fold_left2 f a [b1; ...; bn] [c1; ...; cn] is f (... (f (f a b1 c1) b2 c2) ...) bn cn. Raise Invalid_argument if the two lists are determined to have different lengths. *)
-
-(* val vars : term -> variable list = <fun> *)
-let rec vars t = match t with
-	V v -> [v]
-	| N (sym, []) -> [] 
-	| N (sym, termlist) -> if List.length termlist = 0 then [] else List.fold_left (fun a b -> unification a (vars b)) [] termlist ;;
-
-
-(* val mgu : term -> term -> (variable * term) list = <fun> *)
-(* most general unifier *)
 let rec mgu t1 t2 = match (t1, t2) with
-	(V x, V y) -> if y=x then [] else [(x, V y)]
-	| (V x, N(sym, [])) -> [(x, t2)]
-	| (N(sym, []), V y) -> [(y, t1)]
-	| (N(sym1, []), N(sym2, [])) -> if sym1=sym2 then [] else raise Not_unifiable
-	| (V x, N(sym, termlist)) -> if List.exists (fun tl -> List.mem x (vars tl)) termlist then raise Not_unifiable else [(x, N(sym, termlist))]
-	| (N (sym, termlist), V x) -> if List.exists (fun tl -> List.mem x (vars tl)) termlist then raise Not_unifiable else [(x, N(sym, termlist))]
-	| (N(sym1, termlist1), N(sym2, termlist2)) -> if sym1!=sym2 && List.length termlist2 != List.length termlist2 then raise Not_unifiable
-												else List.fold_left2 (fun s t u-> compose s (mgu (subst t s) (subst u s))) [] termlist1 termlist2;;
+	(C x, C y) -> if y = x then [] else raise Not_unifiable
+	| (V x, C y) | (C y, V x) -> [(x, C y)]
+	| (C _, A _) | (A _ , C _) -> raise Not_unifiable
+	| (V x, V y) -> if x <> y then [(x, V y)] else []
+	| (V x, (A _ as p)) | ((A _ as p), V x) -> if not (occurs x p) then [(x, p)] else raise Not_unifiable
+	| (A(pred1, termlist1), A(pred2, termlist2)) -> if (pred1 = pred2) && (List.length termlist2 = List.length termlist2) then 
+												List.fold_left2 (fun s t u-> unification s (mgu t u)) [] termlist1 termlist2 else raise Not_unifiable;;
+
+let mgu2 a1 a2 = match (a1, a2) with
+ (A(pred1, termlist1), A(pred2, termlist2)) -> if (pred1 = pred2) && (List.length termlist2 = List.length termlist2) then 
+												List.fold_left2 (fun s t u-> unification s (mgu t u)) [] termlist1 termlist2 else raise Not_unifiable;;
+let rec subst term substitution = match term with
+	| C c -> C c
+	| V v -> (try (let x,y = List.find (fun (t1,t2) -> v = t1) substitution in y) with Not_found -> V v)
+	| A(pred, termlist) -> A(pred, List.map (fun t -> subst t substitution) termlist);;
+
+let subst2 atom substitution = match atom with A(pred, termlist) -> A(pred, List.map (fun t -> subst t substitution) termlist);;
+
+let substitutetobody body substitution = List.fold_left (fun x y -> unification x [(subst2 y substitution)]) [] body;;
+
+let rec compose sub1 sub2 =
+	let rec composelist sub1 sub2 sublist =( match (sub1, sub2) with
+	| ([], []) -> sublist
+	| (x, []) | ([], x) -> unification sublist x
+	| (((v1, t1)::s1), ((v2, t2)::s2)) -> if (occurs v2 t1) then composelist s1 s2 (unification sublist [(v1, subst t1 [(v2, t2)]); (v2, t2)])
+										  else composelist s1 s2 (unification [(v1, t1); (v2, t2)] sublist)) in composelist sub1 sub2 [];; 
 
 
-exception Invalid;;
-(* val predicate_to_function_symbol : head -> term = <fun> *)
-let rec predicate_to_function_symbol p = match p with Atm(x, y) -> N (x, y);;
+let rec varsTerm term =  match term with | C c -> []  | V v -> [v] | A(_, termlist) -> List.fold_left (fun x t -> unification x (varsTerm t)) [] termlist ;;
 
-(* val function_to_predicate_symbol : term -> head = <fun> *)
-let rec function_to_predicate_symbol p = match p with N (x, y) -> Atm (x, y)| _-> raise Invalid;;
-
-exception GOALS_TERMINATED
-(* (g: goals; p: program; m: mgu) *)
-let rec process g p m = match g with
- 			[] -> true
-			|g1::grest-> let rec answer g' p' m' = (match p' with
-			  [] -> false
-			  |p1::prest -> (match p1 with
-						  	Fact fact -> (try 
-						  				(let u = mgu (predicate_to_function_symbol fact) (predicate_to_function_symbol g') in
-						  				let goal = List.map ((fun a t -> subst t a) u) (List.map predicate_to_function_symbol grest) in
-						  				if process (List.map function_to_predicate_symbol goal) p (m'@u) then
-						  					(print_string "true\n" ; 
-						  					let wait = read_line() in
-						  						if (wait = ";") then answer g' prest m'
-						  						else raise_notrace GOALS_TERMINATED)
-						  					 else answer g' prest m')
-						  				 with 
-						  					| Not_unifiable -> answer g' prest m';)
-					   	    |Rule rule -> (try
-					   	  			    (let f = fst rule in
-				   						let u = mgu (predicate_to_function_symbol f) (predicate_to_function_symbol g') in
-				   						let goal = unification (List.map ((fun a t -> subst t a) u) (List.map predicate_to_function_symbol grest)) (List.map ((fun a t -> subst t a) u) (List.map predicate_to_function_symbol (snd rule))) in
-				   						if process (List.map function_to_predicate_symbol goal) p (m' @ u) then 
-				   							(print_string "true\n";
-				   							let w = read_line() in 
-				   								if (w=";") then answer g' prest m' else raise GOALS_TERMINATED
-				   							)
-				   							else answer g' prest m')
-					   					with | Not_unifiable -> answer g' prest m'; ))) in answer g1 p m;;
-
-(* execution -> process goals program [] *)
+let rec varsGoals goals = match goals with | [] -> [] | A(_, termlist)::a -> unification (List.fold_left (fun x t -> unification x (varsTerm t)) [] termlist) (varsGoals a) ;;
 
 
+let clausestruct clause = match clause with | Fact h -> (h, []) | Rule (h,b) -> (h, b);;
 
+(* backtracking *)
+let rec process p g answer vars = match g with | [] -> let choice =
+            (let solved = printsubs answer vars in
+            if answer <> [] && vars <> [] then
+                    if not (List.mem solved !solvedvar_pointer) then
+                        let _ = print_string solved in
+                        solvedvar_pointer := solved::!solvedvar_pointer;
+                        read_line()
+                    else ":" else ".")
+        in (if choice = ";" || choice = ":" then false else true)
+    | g1::g' ->  processg1 p g1 g' answer vars
+and processg1 p g1 g' answer vars =
+    let _ = "nothing" in
+    let rec solve pr goal = match pr with
+        | [] -> false
+        | clause::pr' -> ( try
+                let h, b = (clausestruct clause) in
+                let substi = mgu2 goal h in
+                let new_goals = substitutetobody (unification g' b) substi in
+                if process p new_goals (compose answer substi) vars then true
+                else solve pr' goal
+            with Not_unifiable -> solve pr' goal) in solve p g1 ;;
 
-(* examples *)
-let a = N("a", []);; 
-let b = N("b", []);; 
-let c = N("c", []);; 
-let d = N("d", []);; 
-
-let x = V "X";;
-let y = V "Y";;
-
-let f1 = Atm("Loves", [a;b]) 
-let f2 = Atm("Loves", [b;a]) 
-let f3 = Atm("Loves", [a;c])
-let f4 = Atm("Loves", [a;d])
-
-let r1 = (Atm("Loves", [x;y]),[(Atm("Loves", [y;x]))])
-
-let goal = [f1];;
-let prog = [Fact f1; Rule r1];;
-
-process goal prog [];;
-
-let goal1 = [ Atm ("Loves",[a;x])];;
-let prog1 = [Fact f1; Fact f3; Fact f4];;
-
-process goal1 prog1 [];;
+let solver program goals =
+    solvedvar_pointer := [];
+    let var = varsGoals goals in (process program goals [] var) ;;
